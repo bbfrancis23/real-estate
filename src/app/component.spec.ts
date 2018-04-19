da8e49111f9487cb80516fc4e62fd9a53b518d88
Brian Francis <bbfrancis23@gmail.com>
2018-04-19 08:36:50 -0700

    Add AppComponent testing

--- src/app/component.spec.ts
+++ src/app/component.spec.ts
@@ -4,15 +4,17 @@ import { AppComponent } from './component';
 import { MatButtonModule, MatDialogModule, MatTooltipModule, MatIconModule } from '@angular/material';
 import { AeoModule } from './aeo/module';
 import { AppService } from './service';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SettingsDialog } from './settings-dialog/settings-dialog';
+import { AppModule } from './module';
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [AppComponent],
-      imports: [AeoModule, MatButtonModule, MatDialogModule, MatTooltipModule, MatIconModule],
-      providers: [AppService]
+      declarations: [],
+      imports: [AppModule]
     }).compileComponents();
   }));
+
   it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
