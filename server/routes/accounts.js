bf1372983679a6c925873390c020283dc9f5a2b6
Brian Francis <bbfrancis23@gmail.com>
2018-04-20 20:27:55 -0700

    Add Bcrypt hashing for passwords Fix password visability icon

--- server/routes/accounts.js
+++ server/routes/accounts.js
@@ -2,6 +2,7 @@ const {Account, validate } = require('../models/accounts');
 const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcrypt');
 
 /*
 router.get('/', async (req, res) =>{
@@ -18,10 +19,12 @@ router.post('/', async (req, res) =>{
   if(account) return res.status(400).send('Account already registered.');
 
   account = new Account({ email: req.body.email, password: req.body.password });
+  const salt = await bcrypt.genSalt(10);
+  account.password = await bcrypt.hash(account.password, salt);
 
   account = await account.save();
 
-  res.send(account);
+  res.send({email: account.email, _id: account._id});
 });
 
 
