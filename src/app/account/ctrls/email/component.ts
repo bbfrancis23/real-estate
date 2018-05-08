c3944e9b056bd489daebc39db63f6d637d09d6ba
Brian Francis <bbfrancis23@gmail.com>
2018-05-08 09:27:58 -0700

    Update password and Email Controls

--- src/app/account/ctrls/email/component.ts
+++ src/app/account/ctrls/email/component.ts
@@ -1,16 +1,24 @@
-import { Component, Input } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'email-ctrl',
   templateUrl: 'component.html'
 
 })
-export class EmailControl {
-  @Input() emailCtrl: FormControl;
+export class EmailControl implements OnInit {
+
+  readonly EMAIL = { max: 256 };
+
+  email: FormControl;
+
+  ngOnInit() {
+    this.email = new FormControl('', [Validators.required, Validators.email, Validators.maxLength(this.EMAIL.max)]);
+  }
+
 
   getEmailError() {
-    return this.emailCtrl.hasError('required') ? 'You must enter a value'
-      : this.emailCtrl.hasError('email') ? 'Not a valid email' : '';
+    return this.email.hasError('required') ? 'You must enter a value'
+      : this.email.hasError('email') ? 'Not a valid email' : '';
   }
 }
--- src/app/account/ctrls/password/component.ts
+++ src/app/account/ctrls/password/component.ts
@@ -1,18 +1,25 @@
-import { Component, Input } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'password-ctrl',
   templateUrl: 'component.html'
 
 })
-export class PasswordControl {
-  @Input() passwordCtrl: FormControl;
+export class PasswordControl implements OnInit {
+
+  readonly PASSWORD = { min: 4, max: 16, pattern: /^[^\s]+$/ };
+  password: FormControl;
   inputType = 'password';
+
+  ngOnInit() {
+    this.password = new FormControl('', [Validators.required, Validators.minLength(this.PASSWORD.min), Validators.maxLength(this.PASSWORD.max), Validators.pattern(this.PASSWORD.pattern)])
+
+  }
 
   getPasswordError() {
 
-    let password = this.passwordCtrl;
+    let password = this.password;
 
     return password.hasError('required') ? 'You must enter a value.'
       : password.hasError('pattern') ? 'Not a valid password.'
