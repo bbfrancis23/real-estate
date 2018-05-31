60785d5910e9c14fc48c80d36c58adda274f3317
Brian <bbfrancis23@gmail.com>
2018-05-31 08:48:00 -0700

    clean up agentService

--- src/app/agent/service.ts
+++ src/app/agent/service.ts
@@ -1,18 +1,12 @@
 import { Injectable } from '@angular/core';
-import { Headers, Http } from '@angular/http';
-
 import { BehaviorSubject } from 'rxjs';
-
 import { Account } from '../account/account';
-
 import { HttpClient } from '@angular/common/http';
 
 'use strict';
 
 @Injectable()
 export class AgentService {
-
-  readonly headers = new Headers({ 'Content-Type': 'application/json' });
 
   clients: [Account];
   selectedClient: Account;
@@ -25,37 +19,17 @@ export class AgentService {
   readonly currentSelectedClient = this.selectedClientSource.asObservable();
   public changeCurrentSelectedClient(client: Account) { this.selectedClientSource.next(client) }
 
-  constructor(readonly http: HttpClient) {
-
-  }
-
-
+  constructor(readonly http: HttpClient) { }
 
   getClients() {
-
-    return this.http.get<[Account]>('/api/accounts/clients')
-      .toPromise()
-      .then(res => {
-        this.clients = res;
-        this.changeCurrentClients(this.clients);
-
-        //console.log(this.clients[0]);
-        if (this.clients[0]) {
-          //this.changeCurrentSelectedClient(this.clients[0]);
-        }
-        //this.changeCurrentSelectedClient = this.clients[0];
-      })
-      .catch(err => console.log(err))
+    return this.http.get<[Account]>('/api/accounts/clients').toPromise().then(res => {
+      this.clients = res;
+      this.changeCurrentClients(this.clients);
+    }).catch(err => console.log(err))
   }
 
   updateTheme(theme) {
-
-
-    return this.http.post('/api/accounts/theme', { theme: theme })
-      .toPromise()
-      .then(res => {
-      })
-      .catch(err => console.log(err));
+    return this.http.post('/api/accounts/theme', { theme: theme }).toPromise().then(res => { }).catch(err => console.log(err));
   }
 
 }
